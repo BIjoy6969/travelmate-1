38a835a78e21d5791fab3d4ac8ad288c720fe72e
Abu Safwan Md Farhan <abusafwanmdfarhan@gmail.com>
2026-01-06 05:37:12 +0600

    5V

--- api/[...path].js
+++ api/[...path].js
@@ -1,4 +1,6 @@
 const path = require('path');
+
+// Load environment variables from server/.env
 require('dotenv').config({ path: path.resolve(__dirname, '../server/.env') });
 
 const app = require('../server/server.js');
